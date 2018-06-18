8fe5af5e1801fb9fd900067ee2220fcfbba7a8f7
riceo <cube@sneakybox.com>
2018-06-18 15:15:42 +0100

    Homepage Styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,17 +23,14 @@ export default function Index({ data }) {
   return (
     <div>
       <div className="hero">
-        <div className="container">
+        <div className="container" style={{backgroundImage: "url(" + hero + ")"}}>
           <div className="row">
-            <div className="col-xl-6 left">
-              <h1 className="">Go Cloud-Native with Oracle</h1>
-              <h3 className="subheader">
+            <div className="col-xl-7 left">
+              <h1>Go Cloud-Native with Oracle</h1>
+              <div className="subheader">
                 Learn about the latest developments on building modern
                 applications on top of Oracle Cloud Infrastructure
-              </h3>
-            </div>
-            <div className="col-xl-6 right">
-              <img src={hero} />
+              </div>
             </div>
           </div>
         </div>
@@ -46,6 +43,7 @@ export default function Index({ data }) {
 		<div className="card-head" id="kubernetes"><img src={topic_k8s} /></div>
                 <div className="card-body">
                   <h3>Kubernetes</h3>
+	  	  <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
                 </div>
               </div>
             </Link>
@@ -55,6 +53,9 @@ export default function Index({ data }) {
 		<div className="card-head" id="microservices"><img src={topic_microservices} /></div>
                 <div className="card-body">
                   <h3>Microservices</h3>
+	  	  <span>
+		      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porta ex tellus, eu pellentesque sem eleifend at.
+		  </span>
                 </div>
               </div>
             </Link>
@@ -64,6 +65,9 @@ export default function Index({ data }) {
 		<div className="card-head" id="cloudops"><img src={topic_cloudops} /></div>
                 <div className="card-body">
                   <h3>Cloud-Ops</h3>
+		<span>
+		      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porta ex tellus, eu pellentesque sem eleifend at.
+		</span>
                 </div>
               </div>
             </Link>
@@ -73,6 +77,9 @@ export default function Index({ data }) {
 		<div className="card-head" id="serverless"><img src={topic_serverless} /></div>
                 <div className="card-body">
                   <h3>Serverless</h3>
+		  <span>
+		      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porta ex tellus, eu pellentesque sem eleifend at.
+		  </span>
                 </div>
               </div>
             </Link>
@@ -82,10 +89,10 @@ export default function Index({ data }) {
       <div className="customers">
       	<div className="container">
 		<div className="row">
-			<div className="col-xl-6">
-				<h2>A Customer Casestudy</h2>
-				<h3>Learn how customer achieved x with Oracle Cloudnative</h3>
-				<p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porta ex tellus, eu pellentesque sem eleifend at. Morbi vitae massa orci. Maecenas rhoncus felis et ligula dictum ultrices.</strong></p>
+			<div className="col-xl-6 casestudy-text">
+				<h1>A Customer Casestudy</h1>
+				<h2>Learn how customer achieved x with Oracle Cloudnative</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris porta ex tellus, eu pellentesque sem eleifend at. Morbi vitae massa orci. Maecenas rhoncus felis et ligula dictum ultrices.</p>
 				<button type="button" class="btn btn-primary">Read More...</button>
 			</div>
 			<div className="col-xl-6">
